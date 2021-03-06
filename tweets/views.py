1e95d65d609e78e731a1a2d0a2a9c597af0c4d82
vishal <vishal@ndmeaa.com>
2021-03-06 17:59:19 +0530

    Routing and Dynamic routing by @herovish143

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>hello world</h1> Home Page")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(tweet_id, args, kwargs)
+    return HttpResponse(f"hello {tweet_id}")
+
